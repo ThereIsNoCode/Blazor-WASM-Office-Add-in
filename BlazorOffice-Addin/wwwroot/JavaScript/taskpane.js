ad0c88093df9e35d08aae6a5aea47e846f1fa482
ThereIsNoCode <50684592+ThereIsNoCode@users.noreply.github.com>
2022-07-16 01:28:58 -0400

    Add project files.

---
+++ BlazorOffice-Addin/wwwroot/JavaScript/taskpane.js
@@ -0,0 +1,24 @@
+﻿/** @type {OfficeCore.Range} */
+/** @type {Office.Range} */
+/** @type {OfficeExtension.Range} */
+
+//Reads the body of the mail in text format
+function readMail() {
+    let body = Office.context.mailbox.item.body;
+
+    body.getAsync(Office.CoercionType.Text, function (result) {
+        document.getElementById("readLabel").innerHTML = result.value;
+    });
+}
+
+//Replaces the text of the body of the mail with a new text
+function writeMail() {
+    let body = Office.context.mailbox.item.body;
+    body.setAsync(
+        document.getElementById("writeBox").value,
+        { coercionType: Office.CoercionType.Text },
+        function (result) {
+            return;
+        }
+    );
+}
