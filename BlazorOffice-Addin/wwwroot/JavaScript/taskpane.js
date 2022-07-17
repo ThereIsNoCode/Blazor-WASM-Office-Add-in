1ce741812b9eb046ce9ca9d0d14b4ff5c0ec0612
ThereIsNoCode <50684592+ThereIsNoCode@users.noreply.github.com>
2022-07-17 19:10:28 -0400

    Added try and catch to office functions to remind user use add-in website using Outlook

--- BlazorOffice-Addin/wwwroot/JavaScript/taskpane.js
+++ BlazorOffice-Addin/wwwroot/JavaScript/taskpane.js
@@ -4,21 +4,33 @@
 
 //Reads the body of the mail in text format
 function readMail() {
-    let body = Office.context.mailbox.item.body;
-
-    body.getAsync(Office.CoercionType.Text, function (result) {
-        document.getElementById("readLabel").innerHTML = result.value;
-    });
+    try {
+        let body = Office.context.mailbox.item.body;
+        body.getAsync(Office.CoercionType.Text, function (result) {
+            document.getElementById("readLabel").innerHTML = result.value;
+        });
+    }
+    catch (err) {
+        document.getElementById("readLabel").innerHTML =
+            "An error has occured, please make sure you are running this in an Outlook Client. If you are, please check console to read the error message";
+        console.log(err.message);
+    }
 }
 
 //Replaces the text of the body of the mail with a new text
 function writeMail() {
-    let body = Office.context.mailbox.item.body;
-    body.setAsync(
-        document.getElementById("writeBox").value,
-        { coercionType: Office.CoercionType.Text },
-        function (result) {
-            return;
-        }
-    );
+    try {
+        let body = Office.context.mailbox.item.body;
+        body.setAsync(
+            document.getElementById("writeBox").value,
+            { coercionType: Office.CoercionType.Text },
+            function (result) {
+            }
+        );
+    }
+    catch(err) {
+        document.getElementById("writeError").innerHTML =
+            "An error has occured, please make sure you are composing an email when attempting to use the write mail functionality";
+        console.log(err.message);
+    }
 }
